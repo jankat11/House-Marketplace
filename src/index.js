89e18729e8326882e3ad95b3f8cf292274fdde8f
jankat <cankatguven@gmail.com>
2022-10-20 11:55:40 +0300

    navbar add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom"
+import './index.css'
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,5 +11,5 @@ ReactDOM.render(
   document.querySelector("#root")
 )
 
-
+reportWebVitals();
 
